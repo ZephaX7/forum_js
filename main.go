7494e1bd8d8c4d52be26e57b76495ecdce4f8735
EstebanKlapczynski <esteban.klapczynski@ynov.com>
2026-05-28 16:46:30 +0200

    f

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"net/http"
-	"os/exec"
 )
 
 func main() {
 	routes()
-	go exec.Command("cmd", "/c", "start", "http://localhost:8080").Run()
+
 	http.ListenAndServe(":8080", nil)
 }
