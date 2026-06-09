73651a951ec9cf24b4db96c92bde4c73c057d26c
knilaksh <nilakshan.konesalingam@ynov.com>
2026-06-09 14:24:24 +0200

    test

--- main.go
+++ main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
 )
 
 func main() {
 	routes()
 	log.Println("Serveur démarré sur http://localhost:8080")
+
+	// Ouvre le navigateur par défaut après un court délai
+	go func() {
+		time.Sleep(300 * time.Millisecond)
+		url := "http://localhost:8080"
+		if err := openBrowser(url); err != nil {
+			log.Printf("Impossible d'ouvrir le navigateur: %v", err)
+		}
+	}()
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func openBrowser(url string) error {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", "", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	default:
+		cmd = exec.Command("xdg-open", url)
+	}
+	return cmd.Start()
 }
